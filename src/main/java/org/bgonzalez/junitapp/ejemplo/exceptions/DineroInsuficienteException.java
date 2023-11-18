3a624a3fcd273f2443612638d5abcfc76b13cfdf
bgr8594 <bgr8594@gmail.com>
2023-11-17 20:49:00 -0600

    Junit capacitacion
    Aqui voy

--- src/main/java/org/bgonzalez/junitapp/ejemplo/exceptions/DineroInsuficienteException.java
+++
@@ -1,7 +0,0 @@
-package org.bgonzalez.junitapp.ejemplo.exceptions;
-
-public class DineroInsuficienteException extends RuntimeException{
-    public DineroInsuficienteException(String message) {
-        super(message);
-    }
-}
